102581f15256507f214cf2dbf1f311878f2bd5ed
Ashish Padhi <ashish.padhi@bulkmro.com>
2020-06-10 18:56:03 +0530

    Countries Comtainer

---
+++ countries-and-neighbours/src/containers/Countries/Countries.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import styles from './Countries.module.scss'
+
+class Countries extends Component {
+
+    state = {
+        counrties: null
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div className={styles.Countries}>
+                
+            </div>
+        )
+    }
+}
+
+export default Countries
